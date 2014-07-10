77f7d9013474994b2efb75eab4a42ed1959027f3
Daisy Liu <ucsb.daisy@gmail.com>
2014-07-09 23:14:07 -0700

    Created kerpercep.java (Kernel perceptron). Added training file and testing file into the package.

---
+++ NetBeansProjects/ML_01/src/pro3/kerpercep.java
@@ -0,0 +1,14 @@
+package pro3;
+
+/**
+ * This program implements a radial basis function (Gaussian) kernel perceptron.
+ * The program should take four arguments as input: the sigma value of the 
+ * radial basis function (a real value), the name of the positive training data
+ * file, the name of the negative training data file, and the name of the 
+ * testing data file.
+ * 
+ * @author daisy
+ */
+public class kerpercep {
+    
+}
